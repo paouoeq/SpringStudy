647dc6561911c298e83867c5441c479417754de0
ParkSeolah <paouoeq@naver.com>
2023-08-29 18:00:04 +0900

    현재 실행중인 OS 환경변수 출력

---
+++ study/spring05_환경변수/src/main/java/EnvironmentTest.java
@@ -0,0 +1,30 @@
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+public class EnvironmentTest {
+	public static void main(String[] args) {
+		// 운영체제의 환경 변수 얻기(key/value쌍 => Map, Properties)
+		
+		// 1. 일반 자바 클래스
+		Properties props = System.getProperties(); // 모든 환경변수 불러오는 메서드
+		Set<String> keys = props.stringPropertyNames();
+		for(String key : keys) {
+			System.out.println(key + "\t" + props.getProperty(key));
+		}
+		System.out.println("###################################");
+		
+		// 2. 스프링
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+		ConfigurableEnvironment env = ctx.getEnvironment();
+		
+		Map<String, Object> m = env.getSystemProperties(); // 모든 환경변수 불러옴
+		Set<String> keys2 = m.keySet();
+		for(String key : keys2) {
+			System.out.println(key + "\t" + m.get(keys2));
+		}
+	}
+}
